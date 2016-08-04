b80e7527fde900ca1059be6331e5ae4e3ba0c9a9
gronki <gronki@gmail.com>
2016-08-04 08:00:11 +0200

    added convoluntion

--- imutil.f90
+++ imutil.f90
@@ -319,5 +319,36 @@ contains
         end do
     end subroutine
 
+    subroutine convol_dumb(img,krn,imgout)
+        real, intent(in) :: img(:,:), krn(:,:)
+        real, intent(out) :: imgout(size(img,1),size(img,2))
+        integer :: x,y,szx,szy, rx, ry
+        integer ::  x1, y1, x2, y2
+        integer ::  x1k, y1k, x2k, y2k
+        szx = size(img,1)
+        szy = size(img,2)
+        kx = size(krn,1)
+        ky = size(krn,2)
+        if ( mod(kx,2) .eq. 0 .or. mod(ky,2) .eq. 0 ) then
+            stop "Kernel must have uneven dimension"
+        end if
+        rx = floor(kx * 0.5)
+        ry = floor(ky * 0.5)
+        do y=1,szy
+            do x=1,szx
+                x1 = max( 1, x-rx )
+                x1k = 1 + x1 - (x-rx)
+                x2 = min( szx, x+rx )
+                x2k = kx + x2 - (x+rx)
+                y1 = max( 1, y-ry )
+                y1k = 1 + y1 - (y-ry)
+                y2 = min( szy, y+ry )
+                y2k = ky + y2 - (y+ry)
+                imgout(x,y) = sum( krn(x1k:x2k,y1k:y2k) * img(x1:x2,y1:y2) )
+            end do
+        end do
+
+    end subroutine
+
 
 end module
